b0bcd8b8bd60c77e8da376738ca4e060151b7a3a
ibnu muzzakkir <ibnumuzzaakkir@gmail.com>
2017-03-12 13:43:27 +0700

    add top menu component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+import { AlertModule } from 'ng2-bootstrap';
+import { ButtonsModule } from 'ng2-bootstrap/buttons';
+import { TopMenuComponent } from './common/top-menu/top-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopMenuComponent
   ],
   imports: [
+    AlertModule.forRoot(),
+    ButtonsModule.forRoot(),
     BrowserModule,
     FormsModule,
     HttpModule
---
+++ src/app/common/top-menu/top-menu.component.ts
@@ -0,0 +1,11 @@
+import { AfterViewInit, Component, Inject, Renderer } from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser';
+
+@Component({
+  selector: 'top-menu',
+  templateUrl: './top-menu.component.html'
+})
+
+export class TopMenuComponent{
+
+}
