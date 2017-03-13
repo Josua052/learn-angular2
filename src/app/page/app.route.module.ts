436990e6586419e96a0c15936466d1bb83d11f90
Ibnu Muzzakkir <personalict@gmail.com>
2017-03-13 13:46:08 +0700

    fix error on app route module

--- src/app/page/app.route.module.ts
+++ src/app/page/app.route.module.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: 'home', component:HomeComponent },
-  { path: 'about', component: AboutComponent}
+  { path: 'about', component: AboutComponent},
   { path : '', component :HomeComponent}
 ];
 
