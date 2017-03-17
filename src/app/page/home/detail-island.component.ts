a349dc8339fc3bbafc9927b956f70fe97ca255f0
ibnu muzzakkir <ibnumuzzaakkir@gmail.com>
2017-03-17 08:22:11 +0700

    add new json mockup for island on home page

--- src/app/page/home/detail-island.component.ts
+++ src/app/page/home/detail-island.component.ts
@@ -16,7 +16,7 @@ export class DetailIslandComponent implements OnInit{
   ngOnInit(): void {
     this.sub = this.route.params.subscribe(params => {
          this.id = +params['id']; // (+) converts string 'id' to a number
-
+          
          // In a real app: dispatch action to load the details here.
     });
   }
--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, Inject, Renderer } from '@angular/core';
+import { AfterViewInit, Component, Inject, Renderer,OnInit,OnDestroy } from '@angular/core';
 import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
@@ -6,8 +6,29 @@ import { DOCUMENT } from '@angular/platform-browser';
   templateUrl: './home.component.html'
 })
 
-export class HomeComponent{
-  title = 'Belajar Angular 2 Part - 2';
-  indonesiaIslandList = ['Sumatera', 'Jawa', 'Bali', 'Kalimantan','Sulawesi','Papua'];
+export class HomeComponent implements OnInit{
+  title: string;
+  data : any;
+  indonesiaIslandList :string[];
+
+  constructor(){
+
+  }
+  ngOnInit(): void {
+    this.title = 'Belajar Angular 2 Part - 2';
+    this.indonesiaIslandList = ['Sumatera', 'Jawa', 'Bali', 'Kalimantan','Sulawesi','Papua'];
+    let populateIsland = {
+        "data" : [
+            {"id" :"1", "name" : "Sumatera"},
+            {"id" :"2", "name" : "Jawa"},
+            {"id" :"3", "name" : "Bali"},
+            {"id" :"4", "name" : "Kalimantan"},
+            {"id" :"5", "name" : "Sulawesi"},
+            {"id" :"6", "name" : "Papua"}
+        ]
+    }
+    this.data = populateIsland.data;
+    console.log(this.data)
+  }
 
 }
